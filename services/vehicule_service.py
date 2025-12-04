ab67632ed7b8c7c4b56f7c262f87f955c413fb0e
sam3ounbenabdallah-ux <sam3oun.benabdallah@gmail.com>
2025-12-04 21:34:24 +0100

    update

--- services/vehicule_service.py
+++ services/vehicule_service.py
@@ -10,24 +10,30 @@ class VehiculeService:
     def load_all():
         tree = ET.parse(FILE)
         root = tree.getroot()
+
         vehicules = []
+
         for v in root.findall("vehicule"):
             vehicules.append(Vehicule(
                 v.find("matricule").text,
                 v.find("marque").text,
-                int(v.find("capacite").text),
-                float(v.find("prix").text),
-                int(v.find("age").text)
+                v.find("capacite").text,
+                v.find("prix").text,
+                v.find("age").text
             ))
+
         return vehicules
 
     @staticmethod
     def add(v: Vehicule):
         tree = ET.parse(FILE)
         root = tree.getroot()
+
         el = ET.Element("vehicule")
+
         for k, val in v.to_dict().items():
             ET.SubElement(el, k).text = str(val)
+
         root.append(el)
         indent(root)
         tree.write(FILE, encoding="UTF-8", xml_declaration=True)
@@ -36,26 +42,30 @@ class VehiculeService:
     def delete(matricule):
         tree = ET.parse(FILE)
         root = tree.getroot()
+
         for v in root.findall("vehicule"):
             if v.find("matricule").text == matricule:
                 root.remove(v)
                 indent(root)
                 tree.write(FILE, encoding="UTF-8", xml_declaration=True)
                 return True
+
         return False
 
     @staticmethod
     def update(old_mat, new_v: Vehicule):
         tree = ET.parse(FILE)
         root = tree.getroot()
+
         for v in root.findall("vehicule"):
             if v.find("matricule").text == old_mat:
+
                 for k, val in new_v.to_dict().items():
                     v.find(k).text = str(val)
+
                 indent(root)
                 tree.write(FILE, encoding="UTF-8", xml_declaration=True)
                 return True
-        return False
 
-# ⚠️ Créer l’instance ici
+        return False
 vehicule_service = VehiculeService()
